2da687de8d299e09d540ccbc0255a474d107990b
henriquemsouza <henrique.m_s@hotmail.com>
2020-01-08 17:56:07 -0300

    save

---
+++ ProAgil.API/Model/Evento.cs
@@ -0,0 +1,16 @@
+namespace ProAgil.API.Model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+
+        public int Lote { get; set; }
+
+         public string ImageUrl { get; set; }
+
+    }
+}
